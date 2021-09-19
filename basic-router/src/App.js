827e3b491d4baf586f251da4b3b9420f73f9c34c
zuwannn <suwanneethongtha@gmail.com>
2021-09-20 01:08:55 +0700

    Setting up the project

---
+++ basic-router/src/App.js
@@ -0,0 +1,46 @@
+import React, { Fragment } from 'react'
+import './index.css';
+
+export default function App(){
+  return(
+    <main>
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/contact">Contact</a></li>
+        </ul>
+      </nav>
+    </main>
+  )
+}
+
+const FakeText = () => (
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur accusantium accusamus quisquam reiciendis dolorum, ad cumque commodi temporibus error porro.
+  </p>
+)
+
+// Home Page
+const Home = () => (
+  <Fragment>
+    <h1>Home</h1>
+    <FakeText />
+  </Fragment>
+)
+  
+// About Page
+const About = () => (
+  <Fragment>
+    <h1>About</h1>
+    <FakeText />
+  </Fragment>
+)
+  
+// Contact Page
+const Contact = () => (
+  <Fragment>
+    <h1>Contact</h1>
+    <FakeText />
+  </Fragment>
+)
