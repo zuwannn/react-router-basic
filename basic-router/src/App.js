e01254f028bc44453b1d72e9e21a8c149aa9e653
zuwannn <suwanneethongtha@gmail.com>
2021-09-20 11:41:13 +0700

    Redirecting to a 404 page

--- basic-router/src/App.js
+++ basic-router/src/App.js
@@ -18,6 +18,7 @@ export default function App(){
           <Route path="/" exact component={Home}/>
           <Route path="/about/:name" component={About}/>
           <Route path="/contact" component={Contact}/>
+          <Route render={() => <h1>404 : page not found.</h1>} />
         </Switch>
         
       </main>
